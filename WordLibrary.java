af15be6ca62d70d5cf3fc8c0f082d8833d525ae1
Hannah <hannahamitchell99@gmail.com>
2023-03-08 16:25:19 +0200

    added files and started basic outline

---
+++ WordLibrary.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class WordLibrary {
+    ArrayList<String> wordArr;
+
+    public WordLibrary(ArrayList<String> wordArr) {
+        this.wordArr = wordArr;
+    }
+
+
+
+
+}
